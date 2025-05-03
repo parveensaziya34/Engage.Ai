d7c1b8713d30f9b3351fca0d2d5617adf302a504
Saziya Parveen <parveensaziya34@gmail.com>
2025-05-03 17:29:45 +0530

    update

--- engage_ai/frontend_page/urls.py
+++ engage_ai/frontend_page/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from frontend_page.views import login_page,access_page
+from frontend_page.views import login_page,access_page,signup_page,chatbot_page
 urlpatterns = [
     path('login/', login_page),
     path('access/',access_page),
+    path('signup/',signup_page),
+    path('chatbot/',chatbot_page),
+
 
 ]
--- engage_ai/frontend_page/views.py
+++ engage_ai/frontend_page/views.py
@@ -4,9 +4,17 @@ from django.shortcuts import render
 def login_page(request):
     return render(request,'login_screen.html',{})
 
+def signup_page(request):
+    return render(request,'signup.html',{})
+
 
 def dashboard_page(request):
     return render(request,'dashboard.html',{})
 
 def access_page(request):
     return render(request,'access_matrix_admin.html',{})
+
+def chatbot_page(request):
+        return render(request,'chatbot.html',{})
+
+
