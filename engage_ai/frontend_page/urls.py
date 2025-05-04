35636310201fada71acda8933618feca696bada9
Saziya Parveen <parveensaziya34@gmail.com>
2025-05-04 12:27:12 +0530

    update

--- engage_ai/frontend_page/urls.py
+++ engage_ai/frontend_page/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from frontend_page.views import login_page,access_page,signup_page,chatbot_page
 urlpatterns = [
-    path('login/', login_page),
+    path('login/', login_page,name = "signin_page"),
     path('access/',access_page),
-    path('signup/',signup_page),
+    path('signup/',signup_page,name="signup_page"),
     path('chatbot/',chatbot_page),
 
 
--- engage_ai/frontend_page/views.py
+++ engage_ai/frontend_page/views.py
@@ -5,6 +5,23 @@ def login_page(request):
     return render(request,'login_screen.html',{})
 
 def signup_page(request):
+    if request.method == 'POST':
+        print(request.POST)
+        print('form has been submitted')
+        form_name=request.POST["name"]               
+        form_email=request.POST["email"]
+        form_pass=request.POST["password"]
+        form_confirmpass=request.POST["confirm_password"]
+        print(form_name)
+        print(form_email)
+        print(form_pass)
+        print(form_confirmpass)
+        if form_pass == form_confirmpass:
+            print('pass is matched')
+        else:
+            print('pass is not matched')
+
+
     return render(request,'signup.html',{})
 
 
