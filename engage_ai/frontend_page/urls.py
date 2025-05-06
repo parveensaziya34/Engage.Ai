5b675861f535dd5c4db303a3373f386b9c52384f
Saziya Parveen <parveensaziya34@gmail.com>
2025-05-06 18:36:34 +0530

    update

--- engage_ai/frontend_page/urls.py
+++ engage_ai/frontend_page/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from frontend_page.views import login_page,access_page,signup_page,chatbot_page
+from frontend_page.views import login_page,access_page,signup_page,chatbot_page,control_page
 urlpatterns = [
-    path('login/', login_page,name = "signin_page"),
+    path('', login_page,name = "signin_page"),
     path('access/',access_page),
     path('signup/',signup_page,name="signup_page"),
     path('chatbot/',chatbot_page),
+    path('control/',control_page),
+
 
 
 ]
