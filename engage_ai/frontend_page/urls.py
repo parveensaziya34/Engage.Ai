1034cca6d7430b59f5bdf20b55cd5ceb6d9a5ec0
Mehak Singhal <146120019+MEHAKSINGHAL10@users.noreply.github.com>
2025-06-21 08:59:30 +0530

    Update urls.py

--- engage_ai/frontend_page/urls.py
+++ engage_ai/frontend_page/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
-from frontend_page.views import login_page,access_page,signup_page,chatbot_page,control_page,dashboard_page,calender_page,email_page,chatbot_reciever_page
+from frontend_page.views import login_page,access_page,signup_page,chatbot_page, dashboard_page, logout_user,calender_page, control_page
 urlpatterns = [
     path('', login_page,name = "login_page"),
-    path('access/',access_page,name = 'access_page'),
-    path('signup/',signup_page,name="signup_page"),
-    path('chatbot/',chatbot_page,name="chatbot_page"),
-    path('control/',control_page, name='control_page'),
-    path('dashboard/',dashboard_page, name='dahsboard_page'),
-    path('calender/',calender_page, name='calender_page'),
-    path('email/',email_page, name="email_page"),
-    path('chat_reciever/',chatbot_reciever_page, name='chatbot_reciever_page'),
-    path('chatbot/', chatbot_page, name="chatbot_page"),
+    path('signup/', signup_page, name='signup_page'),
 
+    path('dashboard/', dashboard_page, name='dashboard_page'),
+    path('access/', access_page, name='access_page'),
+    path('chatbot/', chatbot_page, name='chatbot_page'),
+    path('logout/', logout_user, name='logout_user'),
+    path('control/', control_page, name='control_page'),
+    path('calender/', calender_page, name='calender_page'),
 
 
 ]
